0c3cdcfd05508da52822dcb256b7bb1c63a0efe0
huynh dai <huynhnguyendai120899@gmail.com>
2022-01-23 10:53:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Test place</p>
+        <p>Test place 1</p>
         <a
           className="App-link"
           href="https://reactjs.org"
